ea7a124d33713c15bfb9e2d43ddea76b4a30332a
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-08-18 19:04:47 +0530

    layout

---
+++ src/Clock.js
@@ -0,0 +1,16 @@
+import './Clock.css';
+
+function Clock() {
+
+  return (
+   <div className='mmbody d-flex justify-content-center align-items-center'>
+    <div className='clock'>
+      <div className='clock-inner-bg'></div>
+      <div className='clock-inner-bg'></div>
+    </div>
+
+   </div>
+  );
+}
+
+export default Clock;
